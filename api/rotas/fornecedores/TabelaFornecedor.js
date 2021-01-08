a17f6058180f5042eb4c9ae88d081953de4f7d33
ThyagoMac <thyago.cormick@gmail.com>
2021-01-08 10:13:52 -0300

    post working

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -3,5 +3,8 @@ const Modelo = require('./ModeloTabelaFornecedores')
 module.exports = {
     listar () {
         return Modelo.findAll() //metodo do sequelize
+    },
+    inserir (fornecedor) {
+        return Modelo.create(fornecedor)
     }
 }
